05332c4034d5ca7f3d756a42c294d2c91586653e
Atharva Nandanwar <Atharva.Nandanwar@Colorado.EDU>
2020-11-22 23:00:49 -0700

    Initial file population - Copied https://docs.silabs.com/bluetooth/latest/code-examples/applications/midi-over-ble - Added write attribute settings to print the MIDI message from PC

---
+++ GNU ARM v7.2.1 - Default/application_properties.d
@@ -0,0 +1,4 @@
+application_properties.o: ../application_properties.c \
+ C:\Users\athar\SimplicityStudio\v4_workspace\soc-empty\platform\bootloader\api/application_properties.h
+
+C:\Users\athar\SimplicityStudio\v4_workspace\soc-empty\platform\bootloader\api/application_properties.h:
---
+++ GNU ARM v7.2.1 - Default/platform/emlib/src/em_assert.d
@@ -0,0 +1,4 @@
+platform/emlib/src/em_assert.o: ../platform/emlib/src/em_assert.c \
+ C:\Users\athar\SimplicityStudio\v4_workspace\soc-empty\platform\emlib\inc/em_assert.h
+
+C:\Users\athar\SimplicityStudio\v4_workspace\soc-empty\platform\emlib\inc/em_assert.h:
