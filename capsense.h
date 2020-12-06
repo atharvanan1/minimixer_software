de051f712bd5cdfad23cbec4ac70bc659ba6328c
Mike Fruge <mifr6526@colorado.edu>
2020-12-05 22:58:05 -0700

    Created Capsense Driver and resolved inclusions 	Not yet tested in hardware 	I will do testing to see if we can work with this code library, or if we need to go lesense based

---
+++ capsense.h
@@ -0,0 +1,23 @@
+/*********************************
+ * File: capsense.h
+ * Project: MiniMixer
+ * Description: Includes definitions used by capsense module
+ *				Includes function prototypes
+ *
+ *
+*********************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
+#define ACMP_CHANNELS 2
+
+#define ACMP_CAPSENSE ACMP1
+// This needs to be filled in by the ports on our cap sensors
+//#define CAPSENSE_CHANNELS       { acmpInputAPORT1XCH0, acmpInputAPORT2XCH1, acmpInputAPORT1XCH2, acmpInputAPORT2XCH3 }
+
+#define CAPSENSE_CHANNELS		{ 0 };
+
+
